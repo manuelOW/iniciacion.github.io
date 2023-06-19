dcd8bff8ef85ed0cebc83b9111502a2d16555fb1
manuelOW <128398955+manuelOW@users.noreply.github.com>
2023-06-19 10:32:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,21 @@ let imagenResultado;
     let qrCode;
 
     async function procesarImagen() {
-      // Obtener la imagen cargada
       const archivo = document.getElementById('imagen').files[0];
       if (!archivo) {
         alert('Por favor, seleccione una imagen para procesar.');
         return;
       }
 
-      // Leer la imagen como objeto de tipo File
       const lector = new FileReader();
       lector.readAsDataURL(archivo);
 
       lector.onload = async () => {
         const imagenBase64 = lector.result.split(',')[1];
 
-        // Obtener un estilo aleatorio
-        const estilos = ["cubismo", "impresionismo", "romanico", "bizantino", "expresionismo"];
+        const estilos = ["Retrato Cubista", "Retrato Impresionista", "Retrato Románico", "Retrato Bizantino", "Retrato Expresionista"];
         estiloSeleccionado = estilos[Math.floor(Math.random() * estilos.length)];
 
-        // Hacer la solicitud a DeepAI Image API
         const formData = new FormData();
         formData.append("image", archivo);
         formData.append("text", new Blob([estiloSeleccionado], { type: "text/plain" }));
@@ -37,14 +33,12 @@ let imagenResultado;
         const data = await response.json();
         console.log(data);
 
-        // Mostrar el resultado en la página web
         imagenResultado = new Image();
         imagenResultado.src = data.output_url;
         imagenResultado.onload = function() {
           document.getElementById('resultado').innerHTML = '';
           document.getElementById('resultado').appendChild(imagenResultado);
-
-          // Mostrar botones para cada estilo posible
+            
           const botonesEstilo = document.createElement('div');
           for (let i = 0; i < estilos.length; i++) {
             const boton = document.createElement('button');
@@ -60,7 +54,7 @@ let imagenResultado;
     }
 
     function verificarEstilo(estiloBoton) {
-      const estilos = ["cubismo", "impresionismo", "romanico", "bizantino", "expresionismo"];
+      const estilos = ["Retrato Cubista", "Retrato Impresionista", "Retrato Románico", "Retrato Bizantino", "Retrato Expresionista"];
       const estiloActual = estilos[estiloBoton];
       if (estiloActual === estiloSeleccionado) {
         alert("¡Has seleccionado el estilo correcto!");
